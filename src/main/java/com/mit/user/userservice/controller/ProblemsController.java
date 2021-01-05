efe769b43ec41b6ce5df313535b0ff5862b666c6
Dmitry <mit.sosnilo@gmail.com>
2021-01-05 03:07:54 +0300

    worked and changed Problem's API

--- src/main/java/com/mit/user/userservice/controller/ProblemsController.java
+++ src/main/java/com/mit/user/userservice/controller/ProblemsController.java
@@ -4,15 +4,18 @@ import com.mit.user.userservice.model.Problem;
 import com.mit.user.userservice.model.ProblemRepository;
 import com.mit.user.userservice.model.User;
 import com.mit.user.userservice.model.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/courses/problems/")
+@RequestMapping("/courses/{courseId}/problems")
 public class ProblemsController {
     private final ProblemRepository problemRepository;
 
@@ -20,38 +23,53 @@ public class ProblemsController {
         this.problemRepository = problemRepository;
     }
 
-    @GetMapping(path = "", produces = "application/json")
-    public Iterable<Problem> getAllProblems() {
-        return problemRepository.findAll();
+    @GetMapping(path = "/", produces = "application/json")
+    public List<Problem> getAllProblemsFromCourse(@PathVariable long courseId) {
+        return problemRepository.getAllProblemsFromCourse(courseId);
     }
 
-    @PostMapping(path = "", consumes = "application/json")
-    public Problem addProblem(@RequestBody Problem problem) {
-        return problemRepository.save(problem);
+    @GetMapping(path = "/{problemId}", produces = "application/json")
+    public ResponseEntity getProblemById(@PathVariable long courseId, @PathVariable long problemId) {
+        Optional<Problem> problem = problemRepository.getProblemById(courseId, problemId);
+        if (problem.isPresent()) {
+            return new ResponseEntity<>(problem, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Course with this id was not found or there is no such task in it", HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping(path = "{courseId}", consumes = "application/json")
+//    @GetMapping(path = "", produces = "application/json")
+//    public Iterable<Problem> getAllProblems() {
+//        return problemRepository.findAll();
+//    }
+
+//    @PostMapping(path = "", consumes = "application/json")
+//    public Problem addProblem(@RequestBody Problem problem) {
+//        return problemRepository.save(problem);
+//    }
+//
+    @PostMapping(path = "/", consumes = "application/json")
     public void addProblemToCourse(@PathVariable long courseId,
                                    @RequestParam(name = "problemId") long problemId) {
         problemRepository.addProblem(courseId, problemId);
     }
+//
+//    @GetMapping(path = "{problemId}", produces = "application/json")
+//    public Optional<Problem> getProblemById(@PathVariable long problemId) {
+//        return problemRepository.findById(problemId);
+//    }
+//
+//    @PutMapping(path = "{problemId}")
+//    public void updateProblem(@PathVariable long problemId, @RequestBody Problem problem) {
+//        Long id = problem.getId();
+//        problemRepository.updateProblem(problem.getId(), problem.getProblemName(), problem.getProblemText(), problem.getProblemTime());
+//    }
+//
+//    @DeleteMapping(path = "{problemId}")
+//    public void deleteProblem(@PathVariable long problemId) {
+//        problemRepository.deleteById(problemId);
+//    }
 
-    @GetMapping(path = "{problemId}", produces = "application/json")
-    public Optional<Problem> getProblemById(@PathVariable long problemId) {
-        return problemRepository.findById(problemId);
-    }
-
-    @PutMapping(path = "{problemId}")
-    public void updateProblem(@PathVariable long problemId, @RequestBody Problem problem) {
-        problemRepository.updateProblem(problem.getId(), problem.getProblemName(), problem.getProblemText(), problem.getProblemTime());
-    }
-
-    @DeleteMapping(path = "{problemId}")
-    public void deleteProblem(@PathVariable long problemId) {
-        problemRepository.deleteById(problemId);
-    }
-
-    @DeleteMapping(path = "{courseId}")
+    @DeleteMapping(path = "/")
     public void deleteProblemFromCourse(@PathVariable long courseId,
                                         @RequestParam(name = "problemId") long problemId) {
         problemRepository.deleteProblemFromCourse(courseId, problemId);
--- src/main/java/com/mit/user/userservice/model/ProblemRepository.java
+++ src/main/java/com/mit/user/userservice/model/ProblemRepository.java
@@ -6,8 +6,23 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 public interface ProblemRepository extends CrudRepository<Problem, Long> {
+
+    @Query(value = "SELECT p.id, p.problem_name,p.problem_text, p.problem_time FROM course co " +
+            "JOIN course_problems cp ON cp.course_course_id = co.course_id " +
+            "JOIN problem p ON p.id = cp.problems_id " +
+            "WHERE co.course_id = :courseId", nativeQuery = true)
+    public List<Problem> getAllProblemsFromCourse(@Param("courseId") long courseId);
+
+    @Query(value = "SELECT p.id, p.problem_name,p.problem_text, p.problem_time FROM course co " +
+            "JOIN course_problems cp ON cp.course_course_id = co.course_id " +
+            "JOIN problem p ON p.id = cp.problems_id " +
+            "WHERE co.course_id = :courseId AND p.id = :problemId", nativeQuery = true)
+    public Optional<Problem> getProblemById(@Param("courseId") long courseId, @Param("problemId") long problemId);
+
     @Modifying
     @Query(value = "INSERT INTO course_problems (course_course_id, problems_id) VALUES (:courseId,:problemId)", nativeQuery = true)
     @Transactional
